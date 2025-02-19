2c0b329b2483ddbbe2cf1f8935ee4abcfe1ac438
Diya Tripathi <tripathidiya673@gmail.com>
2025-02-19 23:03:39 +0530

    error resolved

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/ui/shared/Navbar";
+import Signup from "./components/ui/auth/Signup";
+import Login from "./components/ui/auth/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/ui/auth/Home";
 
-const App = () => {
+const appRouter =createBrowserRouter([
+  {
+  
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/signup',
+    element:<Signup/>
+  }
+])
+
+function App() {
   return (
-    <>
-      <h2 className="text-blue-500">This is a Blue Heading</h2>
-      <h3 className="text-green-500">This is a Green Heading</h3>
-
-    </>
-  )
+   <div>
+  <RouterProvider router ={appRouter}/>
+    </div>
+  );
 }
 
-export default App
+export default App;
